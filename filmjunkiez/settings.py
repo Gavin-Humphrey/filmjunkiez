d62aae9d21d81dab41edc1fdd3b4ef70cc68be95
GmGith <87491753+GmGith@users.noreply.github.com>
2023-12-21 19:56:36 +0100

    update database

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -114,12 +114,8 @@ DATABASES = {
         'NAME': config('DATABASE_NAME', default='Film_Junkiez_db'),
         'USER': config('DATABASE_USER', default='Film_Junkiez_User'),
         'PASSWORD': config('DATABASE_PASSWORD', default='Film_Junkiez_Password'),
-        #'HOST': 'db' if 'DOCKERIZED' in os.environ else 'localhost',
-        #'PORT': config('DJANGO_DB_PORT', default='5432'),
-        'HOST': 'postgres',
-        'PORT':''
-
-        
+        'HOST': 'db' if 'DOCKERIZED' in os.environ else 'localhost',
+        'PORT': config('DJANGO_DB_PORT', default='5432'),    
     }    
 }
 config.debug = True
