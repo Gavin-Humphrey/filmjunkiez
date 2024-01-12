ae28f0a8151002decd7a4dd4bdb4cc5051689cb5
GmGith <87491753+GmGith@users.noreply.github.com>
2024-01-12 12:46:20 +0100

    static setup

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', f'{os.environ.get("DEPLOYED_APP_NAME")}.herokuapp.com']
 
