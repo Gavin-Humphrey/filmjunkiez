69a7dcb226165f0dd071429f1b58f55327d7aa7a
GmGith <87491753+GmGith@users.noreply.github.com>
2024-01-29 14:36:52 +0100

    fixing heroku collectstatic

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -163,14 +163,15 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-if 'CI' in os.environ:
+"""if 'CI' in os.environ:
     # Use Django's built-in static file serving during development
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
 
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
