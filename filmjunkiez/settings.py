4e7f4da8e446ae45c5a492d5a4d48ec9eaeb3421
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-06 00:02:18 +0100

    email verification

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from ast import Import
-from email.policy import default
+#from ast import Import
+#from email.policy import default
 from pathlib import Path
 import os
 from decouple import config, Csv
