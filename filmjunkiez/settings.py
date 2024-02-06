5125cf4925dc8b1dbd8b6bb4850eaf6fa73e4e4c
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-06 16:54:42 +0100

    Refresh environment

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-DEBUG = True
+DEBUG = False
 
 CSRF_COOKIE_SECURE = True
 
