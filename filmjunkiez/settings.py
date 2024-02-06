8de661b4b81b7ad1dd37e4b73d9bb257eaa023cd
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-06 11:48:25 +0100

    email verification

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -44,9 +44,9 @@ CSRF_COOKIE_SECURE = True
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'film-junkiez-be8d3d00a54d.herokuapp.com', f'{os.environ.get("DEPLOYED_APP_NAME")}.herokuapp.com']
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', f'{config("DEPLOYED_APP_NAME", default="default_app_name")}.herokuapp.com']
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+#ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 #ALLOWED_HOSTS = ['film-junkiez-be8d3d00a54d.herokuapp.com']
-#ALLOWED_HOSTS = [config('ALLOWED_HOST', default='default_host')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='default_host')]
 
 
 # Application definition
