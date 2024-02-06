e442f93af36d6f2abe1fc872b62841d9d4863daa
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-06 17:06:02 +0100

    Refresh environment

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -48,7 +48,7 @@ CSRF_COOKIE_SECURE = True
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.herokuapp.com']
 #ALLOWED_HOSTS = ['.herokuapp.com']
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default = '.herokuapp.com, localhost', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default='default_value')
 DEVELOPMENT_HOSTS = config('DEVELOPMENT_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 if DEBUG:
