2b55ddc157dea6b779dbb2967aee7f54aba214e9
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-20 21:31:10 +0100

    Updated DEFAULT_STORAGE

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -186,7 +186,7 @@ else:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+    #DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
