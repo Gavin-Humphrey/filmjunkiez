a8c31fd1093b14913563ab46e1e1b39fce2b5802
GmGith <87491753+GmGith@users.noreply.github.com>
2024-02-21 18:54:30 +0100

    Updated DEFAULT_STORAGE using Cloudinary

--- filmjunkiez/settings.py
+++ filmjunkiez/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'cloudinary_storage',###
+    'cloudinary',###
     "base.apps.BaseConfig",
     "user_follow.apps.UserFollowConfig",
     "rest_framework",
