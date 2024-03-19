538839da7ab9a122eb4419d02d35c509dd874eb8
GmGith <87491753+GmGith@users.noreply.github.com>
2024-03-19 16:17:14 +0100

    Adding Advert Card

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,38 +1,42 @@
-// Menu
 
-const dropdownMenu = document.querySelector(".dropdown-menu");
-const dropdownButton = document.querySelector(".dropdown-button");
+// Wait for the DOM content to be fully loaded before executing JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+  // Menu
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  const dropdownButton = document.querySelector(".dropdown-button");
 
-if (dropdownButton) {
-  dropdownButton.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("show");
-  });
-}
+  if (dropdownButton) {
+    dropdownButton.addEventListener("click", () => {
+      dropdownMenu.classList.toggle("show");
+    });
+  }
 
-// Upload Image
-const photoInput = document.querySelector("#avatar");
-const photoPreview = document.querySelector("#preview-avatar");
-if (photoInput)
-  photoInput.onchange = () => {
-    const [file] = photoInput.files;
-    if (file) {
-      photoPreview.src = URL.createObjectURL(file);
+  // Upload Image
+  const photoInput = document.querySelector("#avatar");
+  const photoPreview = document.querySelector("#preview-avatar");
+  if (photoInput) {
+    photoInput.onchange = () => {
+      const [file] = photoInput.files;
+      if (file) {
+        photoPreview.src = URL.createObjectURL(file);
+      }
+    };
+  }
+
+  // Scroll to Bottom
+  const conversationThread = document.querySelector(".room__box");
+  if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
+
+  // Adjust image object fit
+  const image = document.querySelector('.filmListFilm__image img');
+  if (image) {
+    if (image.naturalWidth < 250 && image.naturalHeight < 150) {
+      image.style.objectFit = 'contain';
+    } else {
+      image.style.objectFit = 'cover';
     }
-  };
-
-// Scroll to Bottom
-const conversationThread = document.querySelector(".room__box");
-if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
-
-
-
-const image = document.querySelector('.filmListFilm__image img');
-
-if (image.naturalWidth < 250 && image.naturalHeight < 150) {
-  image.style.objectFit = 'contain';
-} else {
-  image.style.objectFit = 'cover';
-}
+  }
+});
 
 
 ///Announcement
@@ -40,4 +44,10 @@ if (image.naturalWidth < 250 && image.naturalHeight < 150) {
 function updateAnnouncement(message) {
   var announcementBar = document.getElementById('announcementBar');
   announcementBar.innerText = message;
+}
+
+//JS for removing ad
+function removeAdvert() {
+  var advertCard = document.getElementById('advertCard');
+  advertCard.remove();
 }
